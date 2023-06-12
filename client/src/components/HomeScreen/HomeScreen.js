93a518e25e1ad93de7e6a61c2bdd8129ccfc3dfd
Diman Rikalov <dimanrikalov1@gmail.com>
2023-06-12 18:40:26 +0300

    Created home screen page

---
+++ client/src/components/HomeScreen/HomeScreen.js
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import styles from './HomeScreen.module.css';
+
+export const HomeScreen = () => {
+
+    const navigate = useNavigate();
+
+    const clickHandler = (e) => {
+        navigate(`/${e.target.innerText.toLowerCase()}`);
+    }
+
+    return (
+        <div className={styles.container}>
+            <h1>Welcome!</h1>
+            <div className={styles.buttons}>
+                <a className={styles.button} onClick={clickHandler}>Login</a>
+                <a className={styles.button} onClick={clickHandler}>Register</a>
+            </div>
+
+        </div>
+    )
+}
