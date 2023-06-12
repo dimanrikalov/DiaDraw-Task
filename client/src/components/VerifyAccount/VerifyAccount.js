ce9258f975a040c646ab002091cef1dc656b27b1
Diman Rikalov <dimanrikalov1@gmail.com>
2023-06-12 18:42:03 +0300

    Added verify account functionality

--- client/src/components/VerifyAccount/VerifyAccount.js
+++ client/src/components/VerifyAccount/VerifyAccount.js
@@ -1,15 +1,48 @@
 import React, { useState } from 'react';
 import OTPInput from 'otp-input-react';
 import styles from './VerifyAccount.module.css';
+import ENDPOINTS from '../../endpoints';
+import { useNavigate } from 'react-router-dom';
 
 export function VerifyAccount() {
+	const navigate = useNavigate();
 	const [OTP, setOTP] = useState('');
+
+	const handleSubmit = (e) => {
+		console.log(OTP);
+		fetch(ENDPOINTS.VERIFY, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				code: OTP,
+			}),
+		})
+		.then((res) => res.json())
+		.then((data) => {
+			if (data) {
+				localStorage.clear();
+				localStorage.setItem('id', data);
+				navigate('/user');
+			}
+		});
+	};
+
 	return (
-        <div className={styles.container}>
-        <h1>We sent you a code. Please type it in the field below.</h1>
+		<div className={styles.container}>
+			<h1>We sent you a code. Please type it in the field below.</h1>
 			<OTPInput
-            style={{display: 'flex', justifyContent: 'space-between'}}
-				inputStyles={{ width: 42, height: 64, padding: 0, fontSize: 28, margin: 0, color: 'black', backgroundColor: '#dcdcdc' }}
+				style={{ display: 'flex', justifyContent: 'space-between' }}
+				inputStyles={{
+					width: 42,
+					height: 64,
+					padding: 0,
+					fontSize: 28,
+					margin: 0,
+					color: 'black',
+					backgroundColor: '#dcdcdc',
+				}}
 				value={OTP}
 				onChange={setOTP}
 				autoFocus
@@ -18,7 +51,9 @@ export function VerifyAccount() {
 				disabled={false}
 			/>
 			<div>
-				<button className={styles.submitBtn}>Confirm</button>
+				<button className={styles.submitBtn} onClick={handleSubmit}>
+					Confirm
+				</button>
 			</div>
 		</div>
 	);
